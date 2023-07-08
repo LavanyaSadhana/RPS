f0bd199df7c768416a90bcb9fcfe2f4bfabfbd93
lavanyasadhana <sadhana.lavanya07@gmail.com>
2023-07-08 10:19:26 +0000

    RPS

---
+++ src/components/Rps/index.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+import {MainContainer, RulesBtn} from './styledComponents'
+
+class Rps extends Component {
+  render() {
+    return (
+      <MainContainer>
+        <RulesBtn>Rules</RulesBtn>
+      </MainContainer>
+    )
+  }
+}
+export default Rps
